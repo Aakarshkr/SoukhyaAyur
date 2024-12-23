6339e2a4c4ce288cbd34d776f3a686c1dbe31e6d
Aakarshkr <akarshkr88@gmail.com>
2024-12-23 22:07:26 +0530

    after deployed commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SoukhyaAyur/"
+  base: "/SoukhyaAyur/",
+  build: {
+    assetsInlineLimit: 0, // Ensures images aren't inlined into JS
+  },
 })
